d75e71c3d27f72fca9baaaa17ddd264b7a4a6a39
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2021-12-17 23:38:45 +0100

    add vuetify to dashboard

---
+++ dashboard/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <tasks />
+</template>
+
+<script>
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tasks
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
